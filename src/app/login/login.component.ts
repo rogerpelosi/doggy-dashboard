d5a6663986241b7a6211bb0a37ee1570e0959bf1
Pelosi <roger.pelosi@cgi.com>
2022-05-18 10:56:28 -0400

    login view updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder){}
+
+  loginForm: FormGroup = this.formBuilder.group({
+    username: this.formBuilder.control('',[Validators.required]),
+    password: this.formBuilder.control('', [Validators.required])
+  });
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.loginForm.value['username'], this.loginForm.value['password']);
   }
 
 }
